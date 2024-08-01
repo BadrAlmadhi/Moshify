511cd425277e0c690a83b964825d137a88f27b28
Badr Almadhi <badr.almadhi95@gmail.com>
2024-07-31 19:52:25 -0600

    added collapsible components.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    // go through classes of an element and select class collapsible--expanded
+    this.classList.toggle("collapsible--expanded");
+  })
+);
